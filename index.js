0cd2a25b9ba7dfbf42522944a18e713608a6a412
github username <sharnellgrant@gmail.com>
2021-06-05 17:52:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const getText = document.getElementById('text')
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    return getText.innerHTML = "This is really cool!"
+  });
+
